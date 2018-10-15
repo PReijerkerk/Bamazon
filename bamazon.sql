5b6fbb108a81a84ec07ed5753d6a963fdbe9536b
P Reijerkerk <preijerkerk@gmail.com>
2018-10-15 18:43:04 -0400

    Commented in what each line currently does

--- bamazon.sql
+++ bamazon.sql
@@ -1,9 +1,13 @@
+-- Used to make sure database builds from scratch while coding this assignment
 DROP DATABASE IF EXISTS bamazon_DB;
 
+--Creates the database bamazon_DB
 CREATE DATABASE bamazon_DB;
 
+--Makes sure all data is input into bamazon_DB
 USE bamazon_DB;
 
+--Creates a table called products which will hold the products available on Bamazon
 CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50) NULL,
