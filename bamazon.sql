fa9b4a963258adfa8d0d677a9c78497d2bf1d1f5
P Reijerkerk <preijerkerk@gmail.com>
2018-10-20 15:10:57 -0400

    Updated the bamazon.sql to prevent null fields from being entered into the product table

--- bamazon.sql
+++ bamazon.sql
@@ -10,10 +10,10 @@ USE bamazon_DB;
 -- Creates a table called products which will hold the products available on Bamazon
 CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(50) NULL,
-    department_name VARCHAR(50) NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
-    stock_quantity INT(100) NULL,
+    stock_quantity INT(100) NOT NULL,
     PRIMARY KEY (item_id)
 );
 
